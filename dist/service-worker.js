9a30c7db6d15c3ca6cce41c543aee11893bf357c
lvzhenbang <lvzhenbang@outlook.com>
2019-03-01 20:06:39 +0800

    clear it

---
+++ dist/service-worker.js
@@ -0,0 +1,31 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("http://39.105.223.81:8083/workbox-v3.6.3/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "http://39.105.223.81:8083/workbox-v3.6.3"});
+
+importScripts(
+  "http://39.105.223.81:8083/precache-manifest.dd72818c45d1cdfd5db4b38dd9edd190.js"
+);
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
