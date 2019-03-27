33e0a4adc65eb6db350cc597ddd28170dabe5b1a
lvzhenbang <lvzhenbang@outlook.com>
2019-03-27 12:33:32 +0800

    'publish-gh-pages'

---
+++ dist/precache-manifest.3085ee1696c3029d3bea648a5d5799b1.js
@@ -0,0 +1,57 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd4fa6c6ccb59b4a53a96b4defd7b6f1",
+    "url": "https://lvzhenbang.github.io/webpack4.x-angularjs/dist/assets/base64/aboutus-sprite.png"
+  },
+  {
+    "revision": "088396c93d877429a5e5bd9beb4e9477",
+    "url": "https://lvzhenbang.github.io/webpack4.x-angularjs/dist/favicon-512.png"
+  },
+  {
+    "revision": "8a38e52ef61f90c66502cbcf8d9243cd",
+    "url": "https://lvzhenbang.github.io/webpack4.x-angularjs/dist/favicon-64.png"
+  },
+  {
+    "revision": "5cd8175389e70fbe6f924d9a2b0f2a30",
+    "url": "https://lvzhenbang.github.io/webpack4.x-angularjs/dist/favicon-120.png"
+  },
+  {
+    "url": "https://lvzhenbang.github.io/webpack4.x-angularjs/dist/assets/js/vendor.879424519fbc7b95a38c.js"
+  },
+  {
+    "revision": "f7bc5115b3dce5cf630cef23d01abab1",
+    "url": "https://lvzhenbang.github.io/webpack4.x-angularjs/dist/favicon-384.png"
+  },
+  {
+    "revision": "dd57e1b81c293946d2e806b220d98f20",
+    "url": "https://lvzhenbang.github.io/webpack4.x-angularjs/dist/favicon-192.png"
+  },
+  {
+    "revision": "0e0391b3a4aacaddd67db4f89c5de67c",
+    "url": "https://lvzhenbang.github.io/webpack4.x-angularjs/dist/favicon-152.png"
+  },
+  {
+    "revision": "ea3395a3a603b1c1b8236d3b51e8d4f4",
+    "url": "https://lvzhenbang.github.io/webpack4.x-angularjs/dist/favicon-144.png"
+  },
+  {
+    "url": "https://lvzhenbang.github.io/webpack4.x-angularjs/dist/assets/js/app.88ebde98e08d195b3cef.js"
+  },
+  {
+    "url": "https://lvzhenbang.github.io/webpack4.x-angularjs/dist/assets/js/0.47f9e1da09436e72d4eb.js"
+  },
+  {
+    "url": "https://lvzhenbang.github.io/webpack4.x-angularjs/dist/assets/fonts/kt-GB2312.8a1e9fe86f7a9489ec091ec4b78af185.ttf"
+  },
+  {
+    "url": "https://lvzhenbang.github.io/webpack4.x-angularjs/dist/assets/css/app.88ebde98e08d195b3cef.css"
+  },
+  {
+    "revision": "43b8f7f4bc1c0d5e61a5e0d4eff855e2",
+    "url": "https://lvzhenbang.github.io/webpack4.x-angularjs/dist/assets/base64/sj_news-bg.jpg"
+  },
+  {
+    "revision": "bc11ad3335584dc3639796d70dc641a2",
+    "url": "https://lvzhenbang.github.io/webpack4.x-angularjs/dist/assets/base64/contactus-bg.jpg"
+  }
+];
